4f23328c9c3f61b39d64431fc4ea8cb0b0def42a
sweetamily <972041628@qq.com>
2018-11-14 23:56:10 +0800

    update

--- src/pages/welcome/welcome.vue
+++ src/pages/welcome/welcome.vue
@@ -1,12 +1,58 @@
 <template>
   <div class="page">
-    <img class="logo" src="http://thyrsi.com/t6/400/1540826774x1822611437.jpg">
-    <p>让美好的遇见从帮帮开始</p>
+    <!-- <img class="logo" src="http://thyrsi.com/t6/400/1540826774x1822611437.jpg"> -->
+    <p @click="aa">让美好的遇见从帮帮开始</p>
+    <button open-type="getUserInfo" @getuserinfo="bindinfo">获取用户的信息</button>
   </div>
 </template>
 
 <script>
-  
+export default{
+  beforeCreate: function(){
+    wx.showLoading({
+      title: '加载中',
+      mask:true
+    })
+  },
+  created(){
+    wx.getSetting({
+      success(res) {
+        wx.hideLoading()
+        if(res.authSetting['scope.userInfo'] == undefined || res.authSetting['scope.userInfo'] == false){
+          wx.showModal({
+            title: '提示',
+            content: '请授权，否则无法正常使用'
+          })
+        }else{
+          //跳转到主页面
+          console.log('已授权')
+        }
+        wx.hideLoading()
+      }
+    })
+  },
+  methods: {
+    bindinfo:function(){
+      wx.getUserInfo({
+        success: function (res) {
+          wx.showToast({
+            title:'授权成功'
+          })
+          setTimeout(()=>{
+            console.log('eee')
+          },1500)
+        },
+        fail: function () {
+          wx.showToast({
+            icon:'none',
+            title:'请点击授权',
+            duration: 2000
+          });
+        }
+        })
+    },
+  }
+}  
 </script>
 
 <style scoped>
